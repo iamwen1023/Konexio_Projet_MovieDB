c02ed1d54d87e765ff79324d6dcd5a0fd0903dbe
ifeelbored <ursula771023@gmail.com>
2021-05-21 10:06:54 +0200

    link index2 to film

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-
-function getUrlVars()
-{
-    var vars = [], hash;
+function getUrlVars() {
+    var vars = [],
+        hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
+    for (var i = 0; i < hashes.length; i++) {
         hash = hashes[i].split('=');
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
@@ -15,77 +13,77 @@ function getUrlVars()
 const URL_API_KEY = "b8e16ff25f44004fe2ab5dedc9e0453e";
 const URL_API_MOVIE_SEARCH = "https://api.themoviedb.org/3/search/movie/";
 const URL_IMAGE_PREFIX = "https://image.tmdb.org/t/p/w200";
-$(function () {
+$(function() {
     $searchInput = $("#search-input");
     $searchButton = $("#boutton");
     $resultsList = $(".results-list");
     $errorInputEmpty = $(".input-empty");
     $errorFilmNotFound = $(".film-not-found");
     $pagination = $("#pagination");
-   
-    
-   
-    let displayMovies = function (movies) {
+
+
+
+    let displayMovies = function(movies) {
         let movieToDisplay = "";
+        console.log("movies", movies);
         movies.forEach((movie) => {
-            movieToDisplay += '<div class="d-flex mb-4 movie justify-content-between">';
-            movieToDisplay += '<div class= "class1">';
-            movieToDisplay += movie.original_title ?`<h3>${movie.original_title}</h3>`: " ";
+            movieToDisplay += '<a href="film.html?filmId=' + movie.id + '"><div class="row"><div class="col-8 class1">';
+            movieToDisplay += movie.original_title ? `<h3>${movie.original_title}</h3>` : " ";
             movieToDisplay += movie.release_date ? `<p class="p1"><strong>Date de sortie</strong> : ${movie.release_date} </p>` : "";
             movieToDisplay += movie.overview ? `<p class="synopsis"><strong>Synopsis</strong> : ${movie.overview} </p>` : "";
             movieToDisplay += `</div>`;
-            movieToDisplay += `<div>`;
+            movieToDisplay += `<div class="col-4">`;
             movieToDisplay += movie.poster_path ? `<img src="${URL_IMAGE_PREFIX + movie.poster_path}" alt="image-${movie.id}">` : "";
             movieToDisplay += `</div>`;
-            movieToDisplay += `</div>`;
+            movieToDisplay += `</div></a>`;
         });
         $resultsList.append(movieToDisplay);
         return;
     };
-    
-    let ajaxCall = function (urlWithQuery) {
+
+    let ajaxCall = function(urlWithQuery) {
         $.ajax({
             url: urlWithQuery,
-            success: function (data) {
+            success: function(data) {
                 $resultsList.empty();
                 let movies = data.results;
                 if (movies.length === 0) {
-                   
+
                     $errorInputEmpty.addClass("d-none");
-                   
+
                     $errorFilmNotFound.removeClass("d-none");
-                   
+
                     return;
                 }
-                
+
                 displayMovies(movies);
                 displayPagination(data.page, data.total_pages);
             },
         });
     }
     let queryParams = getUrlVars();
-    let query= queryParams["query"];
+    let query = queryParams["query"];
     let page = queryParams["page"] || "1";
-    if(!query){
+    if (!query) {
         $errorFilmNotFound.addClass("d-none");
-          
-            $errorInputEmpty.removeClass("d-none");
-            $errorInputEmpty.addClass("d-block");
-     console.log("noQuery")      
-        
-    }else{
-        console.log("page",page)
+
+        $errorInputEmpty.removeClass("d-none");
+        $errorInputEmpty.addClass("d-block");
+        console.log("noQuery")
+
+    } else {
+        console.log("page", page)
         ajaxCall(URL_API_MOVIE_SEARCH + `?query=${ queryParams["query"]}&api_key=${URL_API_KEY}&page=${page}`)
     }
-    
-    let onClickChangePage = function (event) {
-        
+
+    let onClickChangePage = function(event) {
+
         let page = event.target.id.substr(5);
         let movieSearched = $searchInput.val();
         let urlWithQuery = `${URL_API_MOVIE_SEARCH}?api_key=${URL_API_KEY}&query=${movieSearched}&language=fr-FR&page=${page}`;
         ajaxCall(urlWithQuery);
     };
-    let displayPagination = function (currentPage, totalPages) {
+    let displayPagination = function(currentPage, totalPages) {
         let paginationToDisplay = "";
         if (totalPages <= 1) {
             return;
@@ -119,18 +117,18 @@ $(function () {
         }
         return;
     }
-   
-    
+
+
     // let onClickFunction = function () {
     //     let movieSearched = $searchInput.val();
     //     let urlWithQuery = `${URL_API_MOVIE_SEARCH}?api_key=${URL_API_KEY}&query=${movieSearched}&language=fr-FR`;
 
     //     if (!movieSearched) {
-           
+
     //         $errorFilmNotFound.addClass("d-none");
-          
+
     //         $errorInputEmpty.removeClass("d-none");
-           
+
     //         return;
     //     }
     //     ajaxCall(urlWithQuery);
